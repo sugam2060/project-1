a56f7869dc06b8d84c162a315867fa8a181521e2
sugam2060 <sugampudasain3@gmail.com>
2025-06-02 19:08:42 +0545

    created filter

--- app/(root)/products/page.tsx
+++ app/(root)/products/page.tsx
@@ -5,7 +5,6 @@ import ProductLoadingSkeleton from '@/components/main/ProductLoadingSkeleton'
 import React, { Suspense } from 'react'
 
 const MainProductPage = async () => {
-  const categories = await fetchCategories()
   return (
     <Container className=''>
       <div className="pt-6 pb-1 px-4 bg-gray-100">
@@ -18,7 +17,7 @@ const MainProductPage = async () => {
             />
           }
         >
-          <ProductGrid number={8} categories={categories as Array<{ category: string }>} className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-center gap-3" />
+          <ProductGrid number={8} className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-center gap-3" />
         </Suspense>
       </div>
     </Container>
