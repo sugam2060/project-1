2e4d4cd9fa4f09c531f44338326341dd4705562e
sugam2060 <sugampudasain3@gmail.com>
2025-06-03 12:12:55 +0545

    Change font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Raleway } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const RalewayFont = Raleway({
+  variable: "--font-raleway",
+  weight: "400",
+  display: "swap", // <- helps avoid FOIT (flash of invisible text)
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${RalewayFont.variable} antialiased`}
       >
         {children}
         <Toaster
